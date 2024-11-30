0c46de38cae8bf9b0b8c82ee89cea6280e03f2e4
Lucas Loureiro Rodrigues <lucas@loureiro.tech>
2024-11-29 23:57:24 -0300

    feat: Hello World

---
+++ hello-world/main.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.28;
+
+contract HelloWorld {
+
+    function SayHello() external pure returns (string memory) {
+        return "Hello World!";
+    }
+}
+
