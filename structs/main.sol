8c93949e0dfe463ad7ff2180c564646d9255c1a6
Lucas Loureiro Rodrigues <lucas@loureiro.tech>
2024-12-31 17:41:15 -0300

    feat: structs exercise start

---
+++ structs/main.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract Structs{
+
+    struct Favorites {
+        uint favorite_number;
+        uint8 birthday;
+        uint8[5] lottery_numbers;
+        string favorite_color;
+    }
+     Favorites[] public userFavorites;
+
+    function addFavorites(uint _favoriteNumber, uint8 _birthday, uint8[5] memory _lotteryNumbers, string memory _favoriteColor ) public{
+        Favorites memory newFavorites = Favorites(
+            _favoriteNumber,
+            _birthday,
+            _lotteryNumbers,
+            _favoriteColor
+        );
+        userFavorites.push(newFavorites);
+    }
+
+
+}
